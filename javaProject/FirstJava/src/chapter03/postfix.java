f3774d50d5642c46c4a27bfdfa5892f040cee9dc
HS0430 <hseok0430@gmail.com>
2022-09-14 16:19:51 +0900

    연산자 예제 코드
    단항 연산자 예제

---
+++ javaProject/FirstJava/src/chapter03/postfix.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class postfix {
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		System.out.println("num1 : " + num1);
+		int num2 = num1++; // num2 = num1 -> num1 = num1 + 1
+						   // num2 = 5 , num1 = 6
+		System.out.println("num1 : " + num1);
+		System.out.println("num2 : " + num2);
+		
+		System.out.println("num1 : " + num1++); // 6
+		
+	}
+}
