769880fad3a201bbf4383e9274274b4920c1d772
HS0430 <hseok0430@gmail.com>
2022-09-15 18:00:31 +0900

    chapter 4 exam
    chapter 4 exam

--- javaProject/FirstJava/src/chapter04/exam/Member.java
+++ javaProject/FirstJava/src/chapter04/exam/Member.java
@@ -16,7 +16,6 @@ public class Member {
 			- 대한민국 성인( 20세 )의 경우 무료로 2년마다 건강검진을 받을 수 있습니다.
 			- 짝수 해에 태어난 사람은 올해가 짝수년이라면 검사 대상이 됩니다.
 			- 40 이상의 경우는 암 검사도 무료로 검사를 할 수 있습니다.
-
 		 */
 
 		Scanner in = new Scanner(System.in);
@@ -29,16 +28,16 @@ public class Member {
 		String strHasBook = null;
 		
 		System.out.print("이름을 입력해 주세요. >>> ");
-		name = in.nextLine();
+		name = in.nextLine();	//문자열 입력받기
 		
 		System.out.print("나이를 입력해 주세요. >>> ");
 //		age = Integer.parseInt(in.nextLine());
-		age = in.nextInt();
+		age = in.nextInt();	//정수형 입력받기
 		
 		in.nextLine();
 		
 		System.out.print("별명을 입력해 주세요. >>> ");
-		nickName = in.next();
+		nickName = in.nextLine();
 		
 		System.out.print("키를 입력해 주세요. >>> ");
 		height = in.nextDouble();
@@ -46,7 +45,7 @@ public class Member {
 		System.out.print("책 보유 여부를 입력해 주세요. >>> ");
 		hasBook = in.nextBoolean();
 		in.nextLine();
-		if(hasBook == true) 
+		if(hasBook == true) // 책 보유여부 확인
 			strHasBook = "보유 O";
 		else
 			strHasBook = "보유 X";
@@ -58,7 +57,7 @@ public class Member {
 		System.out.println("자바 책의 보유 여부 : "+strHasBook);
 		
 		System.out.print("출생년도 4자리를 입력해주세요. >>>");
-		int year = Integer.parseInt(in.nextLine());
+		int year = Integer.parseInt(in.nextLine());	//문자열 입력받은 후 정수형으로 변환
 		
 		flu(year);	//독감예방 접종이 가능한지 여부
 		healthScreening(year);	//건강검진 대상 여부
@@ -68,10 +67,10 @@ public class Member {
 	
 	public static void flu(int date) {
 		
-		int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));
-		int targetYear = (year-date)+1;
+		int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));	//현재 연도 구하기 'yyyy'
+		int targetYear = (year-date)+1;	//현재연도 - 입력연도 +1 나이 구하기
 		
-		if((targetYear < 15 && targetYear > 0) || targetYear >= 65)
+		if((targetYear < 15 && targetYear > 0) || targetYear >= 65)	// 1살 이상 15세 미만, 65세 이상 무료접종 대상자 조건문 
 			System.out.println("무료예방접종이 가능합니다.");
 		else 
 			System.out.println("무료접종 대상이 아닙니다.");
@@ -82,19 +81,19 @@ public class Member {
 //		- 짝수 해에 태어난 사람은 올해가 짝수년이라면 검사 대상이 됩니다.
 //		- 40 이상의 경우는 암 검사도 무료로 검사를 할 수 있습니다.
 		
-		int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));
+		int year = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date()));	//현재 연도 구하기 'yyyy'
 //		int year = 2023;
-		int targetYear = (year-date)+1;
+		int targetYear = (year-date)+1;	//현재연도 - 입력연도 +1 나이 구하기
 		
-		if(targetYear >= 20) {
-			if(date%2==0 && year%2==0)
+		if(targetYear >= 20) {	//20세 이상
+			if(date%2==0 && year%2==0)	//현재 연도 및 태어난연도 짝수 확인 
 				System.out.println("20세 이상 짝수 해 / 짝수년이므로 건강검진 대상자 입니다.");
-			else if(date%2 !=0 && year%2 !=0)
+			else if(date%2 !=0 && year%2 !=0)//현재 연도 및 태어난연도 홀수 확인
 				System.out.println("20세 이상 홀수 해 / 홀수년이므로 건강검진 대상자 입니다.");
 			else
 				System.out.println("이번 해 건강검진 대상자가 아닙니다.");
 			
-			if(targetYear >= 40) {
+			if(targetYear >= 40) {	//40세 이상
 				System.out.println("40세 이상, 무료 암 검사 대상자 입니다.");
 			}
 		}
