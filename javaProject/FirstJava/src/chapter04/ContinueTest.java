afce7046a9ce7e642316391a4d2e6825526a04b2
HS0430 <hseok0430@gmail.com>
2022-09-16 15:43:09 +0900

    반복문 continue exam
    반복문 continue exam

---
+++ javaProject/FirstJava/src/chapter04/ContinueTest.java
@@ -0,0 +1,21 @@
+package chapter04;
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+
+		// 1~9 사이에 홀수 만 출력
+		
+		int num = 0;
+		
+		while(num++ < 10) {
+			
+			if(num%2 == 0)
+			{
+				continue;
+			}
+			System.err.println(num);
+		}
+	}
+
+}
