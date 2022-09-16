1eed1fa9c577bd3f4e824fd5519abdc668595c68
HS0430 <hseok0430@gmail.com>
2022-09-16 15:42:24 +0900

    반복문 while exam
    반복문 while exam

---
+++ javaProject/FirstJava/src/chapter04/WhileLoop.java
@@ -0,0 +1,16 @@
+package chapter04;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		//5번 "JAVA"를 출력하는 코드 작성
+		int count = 0;	// 반복 체크할 변수
+		while(count++ < 5) {
+			System.out.println("JAVA");
+//			count = count +1;
+//			count++;
+		}
+	}
+
+}
