159b6203f1863def4c3da7b3ff9541aa51af774f
HS0430 <hseok0430@gmail.com>
2022-09-28 12:10:56 +0900

    chapter 09 exam
    chapter 09 exam

--- javaProject/FirstJava/src/chapter09/exam/CalTimeChk.java
+++ javaProject/FirstJava/src/chapter09/exam/CalTimeChk.java
@@ -1,29 +1,26 @@
 package chapter09.exam;
 
+import java.time.LocalTime;
+
 public class CalTimeChk {
 
 	public static void main(String[] args) {
 
 //		1~100,000,000까지 더하기하는 연산의 실행 시간을 측정하는 프로그램을 만들어봅시다. 
 		long startTime = System.currentTimeMillis();
-		long sTime = System.nanoTime();
-		System.out.println(startTime);
-		System.out.println(sTime);
+		System.out.println("연산 시작 시간 : " + startTime);
+
 		int sum = 0;
 		for (int i = 1; i <= 100000000; i++) {
 			sum += i;
 		}
+
 		long endTime = System.currentTimeMillis();
-		long eTime = System.nanoTime();
-		System.out.println(endTime);
-		System.out.println(eTime);
+		System.out.println("연산 종료 시간 : " + endTime);
 
 		long checkTime = endTime - startTime;
-		long chkTime = eTime - sTime;
 
 		System.out.println("실행 시간 : " + checkTime);
-		System.out.println("실행 시간 : " + chkTime);
-
-		System.out.println(sum);
+		System.out.println("연산 결과 값 : " + sum);
 	}
 }
