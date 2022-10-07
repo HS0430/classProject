e9d125b25858fb893e0dea08718997315f685fd0
HS0430 <hseok0430@gmail.com>
2022-10-07 12:48:22 +0900

    chapter 13
    chapter 13

---
+++ javaProject/FirstJava/src/chapter13/MyThread.java
@@ -0,0 +1,19 @@
+package chapter13;
+
+// 스레드 클래스 정의 : thread 클래스를 상속해서 run() 메소드를 오버라이딩
+public class MyThread extends Thread{
+
+	@Override
+	public void run() {
+		for(int i=0; i<10; i++) {
+//			try {
+//				Thread.sleep(500);
+				System.out.println("새로운 스레드가 생성되어 실행됩니다.");
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		}
+	}
+
+}
