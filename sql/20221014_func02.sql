9ddceaad1dfb82ff2d3f4c85edafc818e7283205
HS0430 <hseok0430@gmail.com>
2022-10-14 10:43:35 +0900

    20221014_func

--- sql/20221014_func02.sql
+++ sql/20221014_func02.sql
@@ -2,12 +2,14 @@
 --(3) 박지성의총구매액(박지성의고객번호는1번으로놓고작성)
 SELECT SUM(SALEPRICE)
 FROM orders
-WHERE CUSTID = 1;
+WHERE CUSTID = '1';
+--WHERE CUSTID = (SELECT custid FROM customer WHERE name = '박지성');
 
 --(4) 박지성이구매한도서의수(박지성의고객번호는1번으로놓고작성)
 SELECT COUNT(*)
 FROM orders
 WHERE CUSTID = 1;
+--WHERE CUSTID = (SELECT custid FROM customer WHERE name = '박지성');
 
 --2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
 --(1) 마당서점도서의총개수
