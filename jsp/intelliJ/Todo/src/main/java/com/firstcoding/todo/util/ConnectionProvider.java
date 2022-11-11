aac12579c48cb7c807d25e8fd6f44f38000c2962
HS0430 <hseok0430@gmail.com>
2022-11-11 17:55:08 +0900

    2022-11-11
    2022-11-11

---
+++ jsp/intelliJ/Todo/src/main/java/com/firstcoding/todo/util/ConnectionProvider.java
@@ -0,0 +1,26 @@
+package com.firstcoding.todo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+
+//    private Connection conn;
+//
+//    private static ConnectionProvider instance = new ConnectionProvider();
+//
+//    private ConnectionProvider(){
+//
+//    };
+//
+//    public ConnectionProvider  getInstance(){
+//        return instance;
+//    }
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "SCOTT", "tiger");
+
+        return conn;
+    }
+}
